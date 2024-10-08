dddd80615f37d5891987cd08420679cfadd3d878
NanKoYo <92059246+NankoYo@users.noreply.github.com>
2024-10-09 01:30:23 +0800

    Update Footer.js

--- themes/starter/components/Footer.js
+++ themes/starter/components/Footer.js
@@ -125,12 +125,31 @@ export const Footer = props => {
                   <p className='text-base text-gray-7'>
                     Designed and Developed by
                     <a
-                      href='https://github.com/tangly1024/NotionNext'
+                      href='https://vallengine.com/'
                       rel='nofollow noopner noreferrer'
                       target='_blank'
                       className='px-1 text-gray-1 hover:underline'>
-                      NotionNext {siteConfig('VERSION')}
+                      vallengine
                     </a>
+                     <br />
+                      Copyright © 2023 - 现在
+                    <a
+                      href='https://smartpages.nankoyo.com/'
+                      rel='nofollow noopner noreferrer'
+                      target='_blank'
+                      className='px-1 text-gray-1 hover:underline'>
+                      SmartPages {siteConfig('VERSION')}
+                    </a>
+                    All Rights Reserved.
+                      <br />
+                    <div
+                    className="text-base text-gray-7"
+                    id="hitokoto"
+                      />
+                    <div
+                    className="text-base text-gray-7"
+                    id="cdn" 
+                      />
                   </p>
                 </div>
               </div>
