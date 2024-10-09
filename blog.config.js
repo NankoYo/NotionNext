b11040e173d73e3e65b5c2aa0e9c000a36bb68df
NanKoYo <92059246+NankoYo@users.noreply.github.com>
2024-10-10 06:54:07 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -149,7 +149,7 @@ const BLOG = {
     process.env.NEXT_PUBLIC_CUSTOM_RIGHT_CLICK_CONTEXT_MENU_THEME_TAG || true, // 是否显示标签
 
   // 自定义外部脚本，外部样式
-  CUSTOM_EXTERNAL_JS: ['https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js','https://cdn.nankoyo.com/public/cache/blog/js/cf-cdn.js','https://v1.hitokoto.cn/?encode=js&select=%23hitokoto'], // e.g. ['http://xx.com/script.js','http://xx.com/script.js']
+  CUSTOM_EXTERNAL_JS: ['https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js','https://cdn.nankoyo.com/public/cache/blog/js/cf-cdn.js','https://v1.hitokoto.cn/?encode=js&select=%23hitokoto','https://cdn-cookieyes.com/client_data/02d7076f1d133e34f26dc544/script.js'], // e.g. ['http://xx.com/script.js','http://xx.com/script.js']
   CUSTOM_EXTERNAL_CSS: [''], // e.g. ['http://xx.com/style.css','http://xx.com/style.css']
 
   // 侧栏布局 是否反转(左变右,右变左) 已支持主题: hexo next medium fukasawa example
