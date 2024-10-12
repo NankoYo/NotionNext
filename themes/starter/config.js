7e3385bcf19ef15a8cdd45439b913f2ba95ff6a2
NanKoYo <92059246+NankoYo@users.noreply.github.com>
2024-10-12 22:18:26 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -22,7 +22,7 @@ const CONFIG = {
 
 
   // 英雄区配图，如需隐藏，改为空值即可 ''
-  STARTER_HERO_PREVIEW_IMAGE: '/images/starter/hero/hero-image.webp', // 产品预览图 ，默认读取public目录下图片
+  STARTER_HERO_PREVIEW_IMAGE: 'https://p0.meituan.net/csc/2acdf8c5a8b46c8381175c8abf0a11a044787.jpg', // 产品预览图 ，默认读取public目录下图片
   STARTER_HERO_BANNER_IMAGE: '', // hero区下方的全宽图
 
   // 顶部右侧导航暗流
