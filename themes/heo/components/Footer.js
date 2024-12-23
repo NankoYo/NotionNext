828b8b5668cc11270c72a12c51053d7e985d3218
NanKoYo <92059246+NankoYo@users.noreply.github.com>
2024-12-23 10:23:49 +0800

    Add files via upload

--- themes/heo/components/Footer.js
+++ themes/heo/components/Footer.js
@@ -1,14 +1,14 @@
 import { BeiAnGongAn } from '@/components/BeiAnGongAn'
+import CopyRightDate from '@/components/CopyRightDate'
+import PoweredBy from '@/components/PoweredBy'
 import { siteConfig } from '@/lib/config'
 import SocialButton from './SocialButton'
-
-const Footer = ({ title }) => {
-  const d = new Date()
-  const currentYear = d.getFullYear()
-  const since = siteConfig('SINCE')
-  const copyrightDate =
-    parseInt(since) < currentYear ? since + '-' + currentYear : currentYear
-
+/**
+ * 页脚
+ * @returns
+ */
+const Footer = () => {
+  const BEI_AN = siteConfig('BEI_AN')
   return (
     <footer className='relative flex-shrink-0 bg-white dark:bg-[#1a191d] justify-center text-center m-auto w-full leading-6  text-gray-600 dark:text-gray-100 text-sm'>
       {/* 颜色过度区 */}
@@ -29,45 +29,12 @@ const Footer = ({ title }) => {
         id='footer-bottom'
         className='w-full h-20 flex flex-col p-3 lg:flex-row justify-between px-6 items-center bg-[#f1f3f7] dark:bg-[#21232A] border-t dark:border-t-[#3D3D3F]'>
         <div id='footer-bottom-left'>
-          <i className='fas fa-copyright' />2024{' '}NanKoYo丨
-          由{' '}
-          <a
-            href='https://smartpages.nankoyo.com/'
-            className='underline font-bold dark:text-gray-300 '>
-            SmartPages {siteConfig('VERSION')}
-          </a>{' '}
-          丨
-          <a
-            href='https://vallengine.com/'
-            className='underline font-bold dark:text-gray-300 '>
-            Vallengine
-          </a>{' '}
-          丨
-          <a
-            href='https://www.nankoyo.com/'
-            className='underline font-bold dark:text-gray-300 '>
-            南科技术
-          </a>{' '}
-          丨
-          <a
-            href='https://www.stwl.us.kg/'
-            className='underline font-bold dark:text-gray-300 '>
-            晓婷网络
-          </a>{' '}
-          提供云计算服务
-          </div>
-          <br />
-<div
-  className="mx-1 animate-pulse "
-  id="cdn"
-/>
-<div        
-className="mx - 1 animate - pulse "
-id="hitokoto"
-/>
-            
-          <div id='footer-bottom-right'>
-          {siteConfig('BEI_AN') && (
+          <PoweredBy />
+          <CopyRightDate />
+        </div>
+
+        <div id='footer-bottom-right'>
+          {BEI_AN && (
             <>
               <i className='fas fa-shield-alt' />{' '}
               <a href='https://beian.miit.gov.cn/' className='mr-2'>
