b4c2815f615300f9e7216fa09b52802ab2e9f60b
NanKoYo <92059246+NankoYo@users.noreply.github.com>
2024-12-23 22:08:18 +0800

    Update CopyRightDate.js

--- components/CopyRightDate.js
+++ components/CopyRightDate.js
@@ -13,30 +13,33 @@ export default function CopyRightDate() {
     parseInt(since) < currentYear ? since + '-' + currentYear : currentYear
 
   return (
-    <div className='whitespace-nowrap flex items-center gap-x-1'></div>
-      <div className='fas fa-copyright' /></div>
-      <div>2024{' '}NanKoYo丨</div>
-      <div
-        href='https://vallengine.com/'
-        className='underline font-semibold dark:text-gray-300 '>
-        Vallengine
-      </div>
-          丨
-      <div
-            href='https://www.nankoyo.com/'
-            className='underline font-semibold dark:text-gray-300 '>
-            南科技术
-      </div>
-          丨
-          <div
-            href='https://www.stwl.us.kg/'
-            className='underline font-semibold dark:text-gray-300 '>
-            晓婷网络
-         </div>
-          提供云计算服务
-          <div
-            id='cdn' className='underline font-semibold dark:text-gray-300 '>
-          <div id='hitokoto' className='underline font-semibold dark:text-gray-300 '>
-    </div>
+    <span>
+            <div className='whitespace - nowrap flex items - center gap - x - 1'>
+                <i className='fas fa - copyright' />
+                <span>{copyrightDate} NanKoYo丨</span>
+            </div>
+            <div
+                href='https://vallengine.com/'
+                className='underline font - semibold dark:text - gray - 300 '>
+                Vallengine
+            </div>
+            丨
+            <div
+                href='https://www.nankoyo.com/'
+                className='underline font - semibold dark:text - gray - 300 '>
+                南科技术
+            </div>
+            丨
+            <div
+                href='https://www.stwl.us.kg/'
+                className='underline font - semibold dark:text - gray - 300 '>
+                晓婷网络
+            </div>
+            提供云计算服务
+            <div id='cdn' className='underline font - semibold dark:text - gray - 300 '>
+            </div>
+            <div id='hitokoto' className='underline font - semibold dark:text - gray - 300 '>
+            </div>
+        </span>
   )
 }
