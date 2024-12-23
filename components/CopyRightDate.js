cbed1d176cbd8771a60e657117bb9e83ab74671e
NanKoYo <92059246+NankoYo@users.noreply.github.com>
2024-12-23 10:28:38 +0800

    Update CopyRightDate.js

--- components/CopyRightDate.js
+++ components/CopyRightDate.js
@@ -14,13 +14,42 @@ export default function CopyRightDate() {
 
   return (
     <span className='whitespace-nowrap flex items-center gap-x-1'>
-      <i className='fas fa-copyright' />
-      <span>{copyrightDate}</span>
-      <a
-        href={siteConfig('LINK')}
-        className='underline font-semibold dark:text-gray-300 '>
-        {siteConfig('AUTHOR')}
-      </a>
+      <i className='fas fa-copyright' />2024{' '}NanKoYo丨
+          由{' '}
+          <a
+            href='https://smartpages.nankoyo.com/'
+            className='underline font-bold dark:text-gray-300 '>
+            SmartPages {siteConfig('VERSION')}
+          </a>{' '}
+          丨
+          <a
+            href='https://vallengine.com/'
+            className='underline font-bold dark:text-gray-300 '>
+            Vallengine
+          </a>{' '}
+          丨
+          <a
+            href='https://www.nankoyo.com/'
+            className='underline font-bold dark:text-gray-300 '>
+            南科技术
+          </a>{' '}
+          丨
+          <a
+            href='https://www.stwl.us.kg/'
+            className='underline font-bold dark:text-gray-300 '>
+            晓婷网络
+          </a>{' '}
+          提供云计算服务
+          </div>
+          <br />
+          <div
+  className="mx-1 animate-pulse "
+  id="cdn"
+/>
+<div        
+className="mx - 1 animate - pulse "
+id="hitokoto"
+/>
     </span>
   )
 }
