026b8fbc960dbd3ee16e9b5534628bf0eca43b8a
NanKoYo <92059246+NankoYo@users.noreply.github.com>
2024-12-28 22:22:00 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -1,10 +1,10 @@
 const CONFIG = {
-  HEO_HOME_POST_TWO_COLS: false, // 首页博客两列显示，若为false则只显示一列
+  HEO_HOME_POST_TWO_COLS: true, // 首页博客两列显示，若为false则只显示一列
   HEO_LOADING_COVER: true, // 页面加载的遮罩动画
 
   HEO_HOME_BANNER_ENABLE: true,
 
-  HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
+  HEO_SITE_CREATE_TIME: '2022-09-21', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
@@ -23,8 +23,8 @@ const CONFIG = {
   HEO_HERO_TITLE_3: 'NANKOYO.COM',
   HEO_HERO_TITLE_4: '新版上线',
 
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_5: 'Smartpages4.7.13 轻松定制主题',
+  HEO_HERO_TITLE_LINK: 'https://nankoyo.com',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
